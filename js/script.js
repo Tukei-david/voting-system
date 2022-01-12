06cea6d9293ee8f26e82624ccac2b0c3b2495863
David Tukei <dtukei831@gmail.com>
2022-01-12 16:32:41 +0300

    Reducing code repetion

--- js/script.js
+++ js/script.js
@@ -1,45 +1,60 @@
 const btnSubmit = document.getElementById('seat-submit')
+const seatName = document.getElementById('seat-name')
+const viedRole = document.getElementById('seats')
+const speechDescription = document.getElementById('speech')
+const showError = document.querySelector('.error')
 
 btnSubmit.addEventListener('click', (e) => {
     e.preventDefault()
 
-    const seatName = document.getElementById('seat-name')
-    const viedRole = document.getElementById('seats')
-    const speechDescription = document.getElementById('speech')
     // const profileUpload = document.getElementById('profile-upload')
     // TODO Checking if the values are empty
     // validateForm(seatName, speechDescription)
 
-    var showError = document.querySelector('.error')
-
-    if (seatName.value == '') {
-        showError.innerText = 'Please input your full names'
-        showError.classList.remove('hide')
-    }else if (speechDescription.value == '') {
-        showError.innerText = 'Please input your speech, Why your deserve the role'
-        showError.classList.remove('hide')
-    }else {
-        // Appending the values
-        displaySeatInfo(seatName, viedRole, speechDescription)
-    }
-
-    
-    
+    displaySeatInfo(seatName.value, viedRole.value, speechDescription.value)
 })
 
 // TODO Minimize the code used.... Simmplicity
 
-
+/**
+ * * Will clear all inputs when the form is submittted
+ */
 function clearInputs() {
-    const seatName = document.getElementById('seat-name')
-    const viedRole = document.getElementById('seats')
-    const speechDescription = document.getElementById('speech')
 
-    var showError = document.querySelector('.error')
+    seatName.value = ''
+    viedRole.value = ''
+    speechDescription.value = ''
+    showError.classList.add('hide')
 }
 
+// * Will store values the user input from the form
+const officalVying = []
+
+/*
+* * This function will display the user input to the browser
+*/
 function displaySeatInfo(name, role, speech) {
 
+    if (name == '') {
+        showError.innerText = 'Please input your full names'
+        showError.classList.remove('hide')
+    }else if (speech == '') {
+        showError.innerText = 'Please input your speech, Why your deserve the role'
+        showError.classList.remove('hide')
+    }else {
+        // Appending the values
+        storeData(name, role, speech)
+        clearInputs()
+    }
+
+
+}
+
+
+/**
+ * * This function will store and display in the html document
+ */
+function storeData(name, role, speech) {
     var displayViedSeats = document.querySelector('.display-seats')
 
     // TODO add image tag and select the uploaded image
@@ -50,37 +65,21 @@ function displaySeatInfo(name, role, speech) {
 
     var showName = document.createElement('h3')
     showName.classList.add('elect-name')
-    showName.innerText = name.value
+    showName.innerText = name
     wrapperDiv.appendChild(showName)
 
     var showRole = document.createElement('p')
     showRole.classList.add('vied-seat')
-    showRole.innerText = role.value
+    showRole.innerText = role
     wrapperDiv.appendChild(showRole)
 
     var showSpeech = document.createElement('p')
     showSpeech.classList.add('seat-des')
-    showSpeech.innerText = speech.value
+    showSpeech.innerText = speech
     wrapperDiv.appendChild(showSpeech)
 
     console.log(wrapperDiv)
 
     // Append the wrapper div
     displayViedSeats.appendChild(wrapperDiv)
-
 }
-
-
-// function validateForm(name, speech) {
-
-//     var showError = document.querySelector('.error')
-
-//     if (name.value == '') {
-//         showError.innerText = 'Please input your full names'
-//         showError.classList.remove('hide')
-//     }else if (speech.value == '') {
-//         showError.innerText = 'Please input your speech, Why your deserve the role'
-//         showError.classList.remove('hide')
-//     }
-
-// }
